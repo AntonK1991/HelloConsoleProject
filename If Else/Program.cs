411ee5a7a16b8bed77b31964529ed4bcd6121d09
Anton <antonkononoff@yandex.ru>
2022-10-30 18:42:34 +0500

    Added If/Else

---
+++ If Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юля")
+{
+    Console.WriteLine("Ура, это же Юля!");    
+}
+else 
+{
+    Console.Write("Привет ");
+    Console.Write (username);
+    Console.WriteLine("!");
+}
